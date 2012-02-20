6d381a6461166f17d044245e8319faadb3b40dcb
blue5tar <blue5tar@163.com>
2012-02-20 21:54:03 +0800

    server/config.js rename server/config.sample.js

---
+++ server/config.sample.js
@@ -0,0 +1,7 @@
+exports.config = {
+    port: 8080,
+    webPath : '/home/blue5tar/work/nodejs/iNode/app',
+    openAccessLog : false,
+    accessLogPath : '/home/blue5tar/work/nodejs/iNode/server/accesslog',
+    staticFileExpires: 60 // second
+};
