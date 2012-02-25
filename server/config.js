dedfa7f66bc47107cbf43388b0017d207f5eea3b
blue5tar <blue5tar@163.com>
2012-02-25 22:13:29 +0800

    rm server/config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    port: 8080,
-    webPath : '/home/blue5tar/work/nodejs/iNode/app',
-    openAccessLog : true,
-    accessLogPath : '/home/blue5tar/work/nodejs/iNode/server/accesslog',
-    staticFileExpires: 60 // second
-};
